e3375c9f360b7ada11e273a0953bd86f70a79d93
Rajesh W <wraejesh@gmail.com>
2020-07-23 00:52:12 +0530

    Initial Webbot commit

---
+++ bots/dialogAndWelcomeBot.js
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { DialogBot } = require('./dialogBot');
+
+class DialogAndWelcomeBot extends DialogBot {
+    constructor(conversationState, userState, dialog) {
+        super(conversationState, userState, dialog);
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity('Hi! This is SOLIZE assistant bot.仮)We assist you in fast staffing placements. (今後のMenuの在り方によっても変更検討。)');
+                    await dialog.run(context, conversationState.createProperty('DialogState'));
+                }
+            }
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+}
+
+module.exports.DialogAndWelcomeBot = DialogAndWelcomeBot;
